70c2d7ccf7c2d56a1aeac7c3dfe983dfd971ece6
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-08-28 14:55:37 -0500

    updated seed information

--- product_seed.sql
+++ product_seed.sql
@@ -3,7 +3,7 @@ USE bamazon_db;
 INSERT INTO products(product_name,department_name,price,stock_quantity)
 VALUES 
 ('T-shirt','clothing',12.50,100),
-('Jeans','clothing',22.50,,100),
+('Jeans','clothing',22.50,100),
 ('Switch','electronics',300,30),
 ('LED TV','electronics',999.99,15),
 ('Blender','kitchen',100,25),
